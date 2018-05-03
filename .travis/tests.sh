eab03af6e279388d97f7fe3f8acfb94be1e8d8c6
ankitjain28may <ankitjain28may77@gmail.com>
2018-05-03 18:58:39 +0530

    Trying

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -12,9 +12,10 @@ BUILD_CHECK_PHPUNIT=$(grep BUILD_CHECK_PHPUNIT .env | xargs)
 BUILD_CHECK_PHPUNIT=${BUILD_CHECK_PHPUNIT#*=}
 
 if [ $BUILD_CHECK_PHPCS == true ]; then
-    echo "Running Code Sniffer for Code Quality"
+    echo "Running Code Sniffer phpcbf for Code Quality"
     docker exec -it drupal vendor/bin/phpcbf --standard=Drupal -p ./web/themes/contrib ./web/modules/contrib
-    docker exec -it drupal vendor/bin/phpcs --standard=Drupal -p ./web/themes/contrib ./web/modules/contrib
+    echo "Running Code Sniffer phpcs"
+    # docker exec -it drupal vendor/bin/phpcs --standard=Drupal -p ./web/themes/contrib ./web/modules/contrib
 fi
 
 if [ $BUILD_CHECK_PHPUNIT == true ]; then
