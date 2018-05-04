eec072a7eeb70041ab3c1f76b7c0a021ce4db1fb
Ankit Jain <ankitjain28may77@gmail.com>
2018-05-04 21:49:01 +0530

    Travis- Added CI & CD (#1)

---
+++ .travis/tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -o errexit
+
+echo "Running Travis Tests"
+
+# Fetching Env Vars
+BUILD_CHECK_PHPCS=$(grep BUILD_CHECK_PHPCS .env | xargs)
+BUILD_CHECK_PHPCS=${BUILD_CHECK_PHPCS#*=}
+
+BUILD_CHECK_PHPUNIT=$(grep BUILD_CHECK_PHPUNIT .env | xargs)
+BUILD_CHECK_PHPUNIT=${BUILD_CHECK_PHPUNIT#*=}
+
+if [ $BUILD_CHECK_PHPCS == true ]; then
+    echo "Running Code Sniffer phpcbf for Code Quality"
+    docker exec -it drupal vendor/bin/phpcbf --standard=Drupal -p ./web/themes/contrib ./web/modules/contrib
+    echo "Running Code Sniffer phpcs"
+    # docker exec -it drupal vendor/bin/phpcs --standard=Drupal -p ./web/themes/contrib ./web/modules/contrib
+fi
+
+if [ $BUILD_CHECK_PHPUNIT == true ]; then
+    echo "Running phpunit tests"
+fi
